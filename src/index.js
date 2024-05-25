5d585555ed6d5e474f831f78781b1377beeec2ae
buildvate-codebase <contato@buildvate.com>
2024-05-25 14:18:56 -0400

    fix: update carousel scrolling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let nextButton = document.querySelector(".arrow_right > button")
 let indicatorsNumber = document.querySelector(".indicators > .number")
 let indicatorsList = document.querySelector(".indicators > ul")
 let carousel = document.querySelector(".carosel > ul")
+let caroselItem = document.querySelector(".carosel > li")
 
 let carsData = []
 
@@ -19,61 +20,79 @@ let firstPosition = 0
 let lastPosition = carsData.length - 1
 indicatorsNumber.innerHTML = 0
 
+window.addEventListener("load", () => {
+  // setItem(0)
+  const data = carsData.map((car) => {
+    return `
+    <li class="carosel-item" draggable="false"> 
+      <div class="car_img ">
+        <img draggable="false" src="${car.image}" alt="imagem carro" />
+      </div>
+      <div class="content">
+          <p class="car-information">Unico Dono</p>
+          <h2>
+            ${car.name}
+          </h2>
+         
+      </div>
+    </li>
+    `
+  })
+  setIndicators(0)
+  carousel.innerHTML = data.join("")
+})
+
 nextButton.addEventListener("click", () => {
   if (active + 1 > carsData.length - 1) {
     active = 0
-    setItem(active)
+    setScroll(active)
     return
   }
 
   active++
-  setItem(active)
+  setScroll(active)
 })
 prevButton.addEventListener("click", () => {
   if (active - 1 < 0) {
     active = carsData.length - 1
 
-    setItem(active)
+    setScroll(active)
     return
   }
 
   active--
-  setItem(active)
+  setScroll(active)
 })
 
-window.addEventListener("load", () => {
-  setItem(0)
-})
-
-function setItem(active) {
-  console.log("active", active)
-
-  const { name = "", image = "" } = carsData[active]
-  indicatorsNumber.innerHTML = String(active + 1).padStart(2, "0")
+function setScroll(active) {
+  carousel.scrollLeft = (960 * active) + (active * 40)
   setIndicators(active)
-  let item = `
-  <li> 
-    <div class="car_img">
-      <img src="${image}" alt="imagem carro" />
-    </div>
-    <div class="content">
-        <p class="car-information">Unico Dono</p>
-        <h2>
-          ${name}
-        </h2>
-       
-    </div>
-  </li>
-
-`
-  carousel.innerHTML = item
 }
 
 function setIndicators(active) {
-  const indicators = carsData.map((_, index) => {
-    if (index === active)
-      return `<li class="active"></li>`
-    return `<li></li>`
-  }).join("")
+  indicatorsNumber.innerHTML = String(active + 1).padStart(2, "0")
+  const indicators = carsData
+    .map((_, index) => {
+      if (index === active) return `<li class="active"></li>`
+      return `<li></li>`
+    })
+    .join("")
   indicatorsList.innerHTML = indicators
 }
+
+window.addEventListener("keyup", (e) => {
+  e.preventDefault()
+  console.log(e.key)
+  if (e.key === "ArrowRight" || e.key === "ArrowUp") {
+    nextButton.click()
+  }
+  if (e.key === "ArrowLeft" || e.key === "ArrowDown") {
+    prevButton.click()
+  }
+})
+
+carousel.addEventListener("wheel", (e) => {
+  e.preventDefault()
+
+}, { passive: false })
+document.addEventListener("contextmenu", e => e.preventDefault())
